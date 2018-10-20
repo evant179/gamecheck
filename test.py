688ff35287ddc5ee25bb5deae3e24d4bb7b03f7c
Evan Tran <evant179@gmail.com>
2018-10-19 22:26:18 -0700

    integrate pyyaml

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print('hello there')
+import requests
+import yaml
+
+config = yaml.load(open("config.yaml"))
+
+def getReleaseDate(name):
+    print('start request for: ' + name)
+    print(config['apiKey'])
+    print('end request')
+
+
+getReleaseDate("Red Dead Redemption 2")
