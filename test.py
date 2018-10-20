43b781a4a0d63732e97ea339bb9e4d4b8117f120
Evan Tran <evant179@gmail.com>
2018-10-20 12:52:11 -0700

    add api call to search giant bomb

--- test.py
+++ test.py
@@ -1,12 +1,27 @@
+import json
 import requests
 import yaml
 
 config = yaml.load(open("config.yaml"))
+apiKey = config['apiKey']
+
 
 def getReleaseDate(name):
     print('start request for: ' + name)
-    print(config['apiKey'])
+    url = 'https://www.giantbomb.com/api/search'
+    headers = {'user-agent': 'test-script'}
+    payload = {
+        'api_key': apiKey,
+        'format': 'json',
+        'query': name,
+        'resources': 'game',
+        'field_list': 'name'
+    }
+    r = requests.get(url, headers=headers, params=payload)
     print('end request')
 
+    data = json.loads(r.text)  # response comes back as str. convert to dict
+    print(json.dumps(data, indent=2))
 
-getReleaseDate("Red Dead Redemption 2")
+
+getReleaseDate("Red Dead")
