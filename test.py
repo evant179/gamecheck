048d8c74dfb5b5fc041da654658a05cfc3982e96
Evan Tran <evant179@gmail.com>
2018-10-20 13:35:10 -0700

    print release dates for games; add rope dev package

--- test.py
+++ test.py
@@ -6,22 +6,37 @@ config = yaml.load(open("config.yaml"))
 apiKey = config['apiKey']
 
 
-def getReleaseDate(name):
-    print('start request for: ' + name)
+def searchGames(name):
     url = 'https://www.giantbomb.com/api/search'
     headers = {'user-agent': 'test-script'}
+    fieldList = 'name,expected_release_day,expected_release_month,expected_release_year'
     payload = {
         'api_key': apiKey,
         'format': 'json',
         'query': name,
         'resources': 'game',
-        'field_list': 'name'
+        'field_list': fieldList
     }
     r = requests.get(url, headers=headers, params=payload)
-    print('end request')
 
-    data = json.loads(r.text)  # response comes back as str. convert to dict
-    print(json.dumps(data, indent=2))
+    response = json.loads(
+        r.text)  # response comes back as str. convert to dict
+    # print(json.dumps(response, indent=2)) # pretty print json string
+    return response['results']
 
 
-getReleaseDate("Red Dead")
+def printGames(games):
+    for game in games:
+        name = game['name']
+        month = 'NA' if not game['expected_release_month'] else game[
+            'expected_release_month']
+        day = 'NA' if not game['expected_release_day'] else game[
+            'expected_release_day']
+        year = 'NA' if not game['expected_release_year'] else game[
+            'expected_release_year']
+        date = str(month) + '/' + str(day) + '/' + str(year)
+        print('[' + name + '] - [' + date + ']')
+
+
+results = searchGames("Red Dead")
+printGames(results)
