65433b5d99dae4ee4dbb117aa4d50766133561ea
Evan Tran <evant179@gmail.com>
2018-12-01 23:03:39 -0800

    remove twilio library and use REST api instead; use pastebin to store release data; send release data to phone numbers through sms

--- handler.py
+++ handler.py
@@ -4,6 +4,11 @@ import os
 import requests
 
 apiKey = os.environ['GIANT_BOMB_API_KEY']
+pastebin_api_key = os.environ['PASTEBIN_API_KEY']
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+from_phone_number = os.environ['TWILIO_PHONE_NUMBER']
+target_phone_numbers = os.environ['TARGET_PHONE_NUMBERS']
 startTimeDelta = timedelta(days=1)
 endTimeDelta = timedelta(days=7)
 
@@ -16,8 +21,14 @@ def processGameCheck(event, context):
     print(f'Query for games between [{startDate}] and [{endDate}]')
     games = searchGames(startDate, endDate)
     convertedGames = processGames(games)
-    print(f'Converted games: {convertedGames}')
+    body = createSmsBody(convertedGames)
 
+    numbers = target_phone_numbers.split(',')
+    for number in numbers:
+        try:
+            sendSmsMessage(body, number)
+        except Exception as e:
+            print(e)
 
 
 def searchGames(startDate, endDate):
@@ -64,6 +75,46 @@ def processGames(games):
         convertedGames.add(gameInfo)
 
     return sorted(convertedGames)
+
+
+def createSmsBody(convertedGames):
+    text = ''
+    for g in convertedGames:
+        text += f'{g}\n'
+    print(text)
+    pasteBinLink = createPastebin(text)
+    print(pasteBinLink)
+
+    rawLink = transformToRawPastbinLink(pasteBinLink)
+    body = f"Here are this week's video game releases!\n{rawLink}"
+    return body
+
+
+def createPastebin(text):
+    url = 'https://pastebin.com/api/api_post.php'
+    data = {
+        'api_dev_key': pastebin_api_key,
+        'api_option': 'paste',
+        'api_paste_code': text
+    }
+    response = requests.post(url, data=data)
+    print(response)
+    return response.text
+
+
+def transformToRawPastbinLink(link):
+    index = link.find('m/')
+    index += 1
+    rawLink = link[:index] + '/raw' + link[index:]
+    print(rawLink)
+    return rawLink
+
+
+def sendSmsMessage(body, targetPhoneNumber):
+    url = f'https://api.twilio.com/2010-04-01/Accounts/{account_sid}/Messages.json'
+    data = {'From': from_phone_number, 'To': targetPhoneNumber, 'Body': body}
+    response = requests.post(url, data=data, auth=(account_sid, auth_token))
+    print(response)
 
 
 if __name__ == "__main__":
