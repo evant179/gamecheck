6e1c1a58f660f60851d91acb7699a700242e07a8
Evan Tran <evant179@gmail.com>
2018-12-03 22:51:08 -0800

    add unit test for transform_to_raw_pastbin_link

--- test_handler.py
+++ test_handler.py
@@ -1,5 +1,13 @@
+import handler
+
 def capital_case(x):
     return x.capitalize()
 
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
+
+def test_transform_to_raw_pastbin_link():
+    link = 'https://pastebin.com/test'
+    raw_link = handler.transformToRawPastbinLink(link)
+    print(f'{link} transformed to {raw_link}')
+    assert raw_link == 'https://pastebin.com/raw/test'
